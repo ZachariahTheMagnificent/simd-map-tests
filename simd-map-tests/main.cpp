0009e02a3bd23866637f14c1f8eafbd68f2509b7
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2018-11-22 23:58:17 +0800

    Implemented component

--- simd-map-tests/main.cpp
+++ simd-map-tests/main.cpp
@@ -17,7 +17,61 @@ int main()
 	auto eng = std::mt19937_64{524};
 	auto random_float = std::uniform_real_distribution<float>{-15, 15};
 
+	auto position_x = array<float>{};
+	auto position_y = array<float>{};
+	auto position_z = array<float>{};
+
+	auto velocity_x = array<float>{};
+	auto velocity_y = array<float>{};
+	auto velocity_z = array<float>{};
+
+	auto acceleration_x = array<float>{};
+	auto acceleration_y = array<float>{};
+	auto acceleration_z = array<float>{};
+
+	position_x.resize(total_gameobjects);
+	position_y.resize(total_gameobjects);
+	position_z.resize(total_gameobjects);
+
+	velocity_x.resize(total_gameobjects);
+	velocity_y.resize(total_gameobjects);
+	velocity_z.resize(total_gameobjects);
+
+	acceleration_x.resize(total_gameobjects);
+	acceleration_y.resize(total_gameobjects);
+	acceleration_z.resize(total_gameobjects);
+
+	for(auto index = 0; index < total_gameobjects; ++index)
+	{
+		position_x[index] = random_float(eng);
+		position_y[index] = random_float(eng);
+		position_z[index] = random_float(eng);
+
+		velocity_x[index] = random_float(eng);
+		velocity_y[index] = random_float(eng);
+		velocity_z[index] = random_float(eng);
+
+		acceleration_x[index] = random_float(eng);
+		acceleration_y[index] = random_float(eng);
+		acceleration_z[index] = random_float(eng);
+	}
+
 	const auto start_point = std::chrono::steady_clock::now();
+
+	for(auto index = 0; index < total_gameobjects; ++index)
+	{
+		acceleration_x[index] += acceleration * delta_time;
+		acceleration_y[index] += acceleration * delta_time;
+		acceleration_z[index] += acceleration * delta_time;
+
+		velocity_x[index] += acceleration_x[index] * delta_time;
+		velocity_y[index] += acceleration_y[index] * delta_time;
+		velocity_z[index] += acceleration_z[index] * delta_time;
+
+		position_x[index] += velocity_x[index] * delta_time;
+		position_y[index] += velocity_y[index] * delta_time;
+		position_z[index] += velocity_z[index] * delta_time;
+	}
 
 	const auto end_point = std::chrono::steady_clock::now();
 
